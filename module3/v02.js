372ce73639fdfc35f201195ef026f95883173c62
rejaur2287 <rezaurewubd@gmail.com>
2026-04-14 12:23:01 +0600

    Basic Class constructor and methods refresher

---
+++ module3/v02.js
@@ -0,0 +1,52 @@
+// * Stateful Function
+
+/* let count = 0;
+const counter = (amount) => {
+  count = count + amount;
+  return count;
+};
+
+console.log(counter(3));
+console.log(counter(2)); */
+
+// * Function with Closure will not go with this for DSA because this is JS specific
+
+/* const createCounter = () => {
+  let count = 0;
+
+  return (amount) => {
+    count = count + amount;
+
+    return count;
+  };
+};
+
+const counter = createCounter();
+console.log(counter(3));
+console.log(counter(5)); */
+
+// * Object creation with class
+
+class Counter {
+  constructor(count) {
+    this.count = count;
+  }
+  add(amount) {
+    this.count = this.count + amount;
+  }
+  print() {
+    console.log(this.count);
+  }
+}
+
+const counter1 = new Counter(0);
+
+counter1.add(3);
+counter1.add(2);
+counter1.print();
+
+const counter2 = new Counter(10);
+
+counter2.add(20);
+counter2.add(30);
+counter2.print();
