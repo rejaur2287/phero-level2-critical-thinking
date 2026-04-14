76c298817ccc27b3676895d23e51fa67308bf42d
rejaur2287 <rezaurewubd@gmail.com>
2026-04-14 11:42:29 +0600

    Stateless vs Stateful

---
+++ module3/v01.js
@@ -0,0 +1,26 @@
+// * Stateless vs Stateful
+
+// * Function Stateless
+/* const counter = (amount) => {
+  let count = 0;
+  count = count + amount;
+  return count;
+};
+
+console.log(counter(3));
+console.log(counter(2)); */
+
+// * Object Stateful
+/* const counter = {
+  count: 0,
+  add(amount) {
+    this.count = this.count + amount;
+  },
+  print() {
+    console.log(this.count);
+  },
+};
+
+counter.add(3);
+counter.add(2);
+counter.print(); */
